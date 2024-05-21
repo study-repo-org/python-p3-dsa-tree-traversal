f1849a6e7469df445c559afddc54ffa0185d6d89
Mohamed Ahmed <ma07041705@gmail.com>
2024-05-21 09:39:13 +0300

    Tree Traversal

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,17 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        return self._get_element_by_id_helper(self.root, id)
+
+    def _get_element_by_id_helper(self, node, id):
+        if node is None:
+            return None
+        if node.get('id') == id:
+            return node
+        for child in node.get('children', []):
+            result = self._get_element_by_id_helper(child, id)
+            if result is not None:
+                return result
+        return None
